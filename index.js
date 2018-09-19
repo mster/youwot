1c08521b77c6c5dc056fe7e415b4b0a9a7d2f6bb
michaelsterpka <michaelsterpka@gmail.com>
2018-09-19 00:00:38 -0700

    clean-up.js added to remove files after rendering, or after an error

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 'use strict'
 
 const edit = require('./lib/edit')
+const cleanUp = require('./util/clean-up')
+const format = require('./util/format')
 
 exports.create = create
 
@@ -14,6 +16,17 @@ class YouWot {
     }
 
     replaceAudioFromVideo(videoId, audioId, fn) {
-        edit.replaceAudioFromVideo(videoId, audioId, fn)
+        edit.replaceAudioFromVideo(videoId, audioId, (path, err) => {
+            // remove download files
+            cleanUp.remove(format.formatWritePath(videoId, '*'))
+            cleanUp.remove(format.formatWritePath(audioId, '*'))
+
+            //remove temp files
+            cleanUp.remove(format.formatTempPath(videoId, '*'))
+            cleanUp.remove(format.formatTempPath(audioId, '*'))
+
+            if(err) fn(err)
+            else fn(path)
+        })
     }
 }
